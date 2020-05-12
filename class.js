9943989b5d498b892b05be860df5b9af87943a69
albo1996 <borovikav96@gmail.com>
2020-05-12 15:08:25 +0700

    Update class.js

--- class.js
+++ class.js
@@ -1,26 +1,47 @@
 class Student {
-    constructor(params) {
-       this.fullName = params.fullName;
-       this.university = params.university;
-       this.course = params.course;
-       this.birthDate = params.birthDate;
-       this.photoUrl = params.photoUrl;
-    }
-    
+   constructor(params) {
+      this.fullName = params.fullName;
+      this.university = params.university;
+      this.course = params.course;
+      this.birthDate = params.birthDate;
+      this.photoUrl = params.photoUrl;
    }
    
-   const studentArr = [
+  }
+  function appendStudentBlock(student) {
+     let div = document.createElement('div');
+     div.setAttribute('class', 'person');
+     let img = document.createElement('img');
+     img.setAttribute('class', 'ava');
+     img.src = this.photoUrl;
+     div.append(img);
+     let div1 = document.createElement('div');
+     div1.setAttribute('class','name');
+     div1.innerHTML = `${this.fullName}`;
+     div.append(div1);
+     let span = document.createElement('span');
+     span.setAttribute('class','university');
+     span.innerHTML = `${this.university}`;
+     div.append(span);
+
+
+
+
+
+  }
+  
+  const studentArr = [
 {
 fullName: 'Маша Иванова',
 university: 'УГАТУ',
 course: 2,
 birthDate: new Date(2000, 0, 1),
-photoUrl: '/photo1'
+photoUrl: 'img/ava02.jpg'
 }
 ];
 studentArr.forEach((item) => {
 const student = new Student(item);
+console.log(student);
 appendStudentBlock(student);
-
 });
 
